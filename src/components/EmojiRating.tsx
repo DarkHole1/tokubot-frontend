e9fb493b23b5194eba29d734d4d6977e6d5767a9
DarkHole1 <dark.hole1@yandex.ru>
2024-07-08 15:08:40 +0300

    Implement rating

--- src/components/EmojiRating.tsx
+++ src/components/EmojiRating.tsx
@@ -1,7 +1,56 @@
-import { Component } from "solid-js";
+import { Component, createEffect, createSignal, Index, Show } from "solid-js";
+
+import styles from "../App.module.css";
+
+type Rating =
+  | {
+      status: "loading";
+    }
+  | {
+      status: "loaded";
+      rating: { name: string; count: number }[];
+    };
 
 const EmojiRating: Component = () => {
-  return <></>;
+  const [rating, setRating] = createSignal<Rating>({
+    status: "loading",
+  });
+
+  createEffect(async () => {
+    setRating({
+      status: "loading",
+    });
+
+    const res = await fetch(`/api/emoji/rating`);
+    const data = await res.json();
+
+    setRating({
+      status: "loaded",
+      rating: data,
+    });
+  });
+
+  const getRating = () => {
+    const r = rating();
+    if (r.status == "loaded") {
+      return r.rating;
+    }
+  };
+
+  return (
+    <Show when={getRating()} fallback={<div>Loading...</div>}>
+      {(rating) => (
+        <Index each={rating()}>
+          {(row) => (
+            <div class={styles["emoji-row"]}>
+              <div class={styles["name"]}>{row().name}</div>
+              <div>{row().count}</div>
+            </div>
+          )}
+        </Index>
+      )}
+    </Show>
+  );
 };
 
 export default EmojiRating;
