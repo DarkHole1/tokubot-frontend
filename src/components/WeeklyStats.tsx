461db623279b0413f1ed39ad6810cce0ff5eeb6a
DarkHole1 <dark.hole1@yandex.ru>
2024-08-22 15:43:47 +0300

    Add stats fetching

--- src/components/WeeklyStats.tsx
+++ src/components/WeeklyStats.tsx
@@ -1,9 +1,48 @@
-import { Component } from "solid-js";
+import { Component, createEffect, createSignal } from "solid-js";
+
+type Stats =
+  | {
+      status: "loading";
+    }
+  | {
+      status: "loaded";
+      stats: StatsEntry[];
+    };
+
+type StatsEntry = {
+  plannedMinutes: number;
+  watchedMinutes: number;
+  date: Date;
+};
+
+const getStats = (stats: Stats) =>
+  stats.status == "loaded" ? stats.stats : null;
 
 const WeeklyStats: Component<{
   userId: number;
-}> = () => {
-  return <div>Hello</div>;
+}> = (props) => {
+  const [stats, setStats] = createSignal<Stats>({
+    status: "loading",
+  });
+
+  createEffect(async () => {
+    setStats({
+      status: "loading",
+    });
+
+    const res = await fetch(`/api/stats/weekly?id=${props.userId}`);
+    const data = await res.json();
+
+    setStats({
+      status: "loaded",
+      stats: data.map((entry: any) => ({
+        ...entry,
+        date: new Date(entry.date),
+      })),
+    });
+  });
+
+  return <pre>{getStats(stats())?.at(0)?.date.constructor.name}</pre>;
 };
 
 export default WeeklyStats;
